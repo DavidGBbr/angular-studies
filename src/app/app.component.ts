e1a70727cabc9d7e121d0069e348974f9d5cd646
davidGBbr <davidgomesbrigido@gmail.com>
2023-08-25 15:30:13 -0300

    ng-content

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,17 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-    <app-diretivas-atributos></app-diretivas-atributos>
+    <app-diretivas-atributos>
+      <h1>Aulas de Diretivas Atributo</h1>
+      <h3>Final do Bloco</h3>
+      <hr />
+    </app-diretivas-atributos>
+
+    <app-diretivas-atributos>
+      <h1>Segundo bloack</h1>
+      <h3>Final do segundo Bloco</h3>
+      <hr />
+    </app-diretivas-atributos>
     <router-outlet></router-outlet>
   `,
 })
