1a923b0b962f261dd2e124c260e54457910fa30e
davidGBbr <davidgomesbrigido@gmail.com>
2023-08-26 22:18:59 -0300

    @Input

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,16 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-    <app-new-component></app-new-component>
+    <app-input [contador]="addValue"></app-input>
+    <button (click)="add()">Add</button>
     <router-outlet></router-outlet>
   `,
 })
 export class AppComponent implements OnInit {
+  public addValue: number = 0;
+  public add = () => {
+    this.addValue += 1;
+  };
   constructor() {}
   ngOnInit(): void {}
 }
