a1affa53e2936173bf5849f3397d835bbe43b071
davidGBbr <davidgomesbrigido@gmail.com>
2023-09-04 18:33:36 -0300

    Subscribe

--- src/app/services/food-list.service.ts
+++ src/app/services/food-list.service.ts
@@ -1,11 +1,9 @@
-import { Injectable } from '@angular/core';
+import { EventEmitter, Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root',
 })
 export class FoodListService {
-  constructor() {}
-
   private list: Array<string> = ['Banana', 'Arroz', 'Ovo', 'Pizza'];
 
   public foodList() {
@@ -13,6 +11,13 @@ export class FoodListService {
   }
 
   public foodListAdd(value: string) {
+    this.foodListAlert(value);
     return this.list.push(value);
+  }
+
+  public emitEvent = new EventEmitter();
+
+  public foodListAlert(value: string) {
+    return this.emitEvent.emit(value);
   }
 }
