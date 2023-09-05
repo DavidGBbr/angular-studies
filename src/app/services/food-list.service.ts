9026922016233a50fe5c00a8004acfacf6ea8548
davidGBbr <davidgomesbrigido@gmail.com>
2023-09-05 15:38:58 -0300

    Observable + http-Get

--- src/app/services/food-list.service.ts
+++ src/app/services/food-list.service.ts
@@ -1,4 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { EventEmitter, Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Foodlist } from '../module/foodlist';
 
 @Injectable({
   providedIn: 'root',
@@ -6,8 +9,19 @@ import { EventEmitter, Injectable } from '@angular/core';
 export class FoodListService {
   private list: Array<string> = ['Banana', 'Arroz', 'Ovo', 'Pizza'];
 
-  public foodList() {
-    return this.list;
+  // public foodList() {
+  //   return this.list;
+  // }
+
+  constructor(private http: HttpClient) {}
+
+  private url: string = 'http://localhost:3000/';
+
+  public foodList(): Observable<Foodlist> {
+    return this.http.get<Foodlist>(`${this.url}list-food`).pipe(
+      (res) => res,
+      (error) => error
+    );
   }
 
   public foodListAdd(value: string) {
--- src/app/shared/food-list/food-list.component.ts
+++ src/app/shared/food-list/food-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Foodlist } from 'src/app/module/foodlist';
 import { FoodListService } from 'src/app/services/food-list.service';
 
 @Component({
@@ -7,12 +8,15 @@ import { FoodListService } from 'src/app/services/food-list.service';
   styleUrls: ['./food-list.component.scss'],
 })
 export class FoodListComponent implements OnInit {
-  public foodList: Array<string> = [];
+  public foodList: Foodlist | any;
 
   constructor(private foodListService: FoodListService) {}
 
   ngOnInit(): void {
-    this.foodList = this.foodListService.foodList();
+    this.foodListService.foodList().subscribe({
+      next: (res) => (this.foodList = res),
+      error: (err) => console.log(err),
+    });
     this.foodListService.emitEvent.subscribe((res) =>
       alert(`Olha você adicionou => ${res}`)
     );
